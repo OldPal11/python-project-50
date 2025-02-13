43b2eb41f810d6169465962f8da1f6c7c02bfc8c
OldPal11 <oldpal11.es@gmail.com>
2025-02-13 15:56:25 +0100

    change the ways to directories in tests

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -2,10 +2,11 @@ import os
 
 from gendiff import generate_diff
 
+BASE_DIR = Path(__file__).parent / "fixtures"
 
 def test_generate_diff_json():
-    first_file = os.path.expanduser('~/python-project-50/fixtures/file1.json')
-    second_file = os.path.expanduser('~/python-project-50/fixtures/file2.json')
+    first_file = BASE_DIR / 'file1.json'
+    second_file = BASE_DIR / 'file2.json'
     expected = '''{
     - follow: False
       host: hexlet.io
--- tests/test_gendiff_yaml.py
+++ tests/test_gendiff_yaml.py
@@ -2,10 +2,11 @@ import os
 
 from gendiff import generate_diff
 
+BASE_DIR = Path(__file__).parent / "fixtures"
 
 def test_generate_diff_yaml():
-    first_file = os.path.expanduser('~/python-project-50/fixtures/file1.yaml')
-    second_file = os.path.expanduser('~/python-project-50/fixtures/file2.yaml')
+    first_file = BASE_DIR / 'file1.yaml'
+    second_file = BASE_DIR / 'file2.yaml'
     expected = '''{
     - follow: False
       host: hexlet.io
